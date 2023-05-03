1d412bf2f5a438b62b53d20c27d718c2dd64229a
Ruslan <ru@skylinehealthgroup.com>
2023-05-03 19:41:53 +0300

    add green_tariff_decoration function to script.js and fix html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,33 @@ const showFormButton = document.querySelector('.header-show-form__button');
 const headerConsultationForm = document.querySelector('.header-consultation-form');
 const headerSuccessfully = document.querySelector('.header-consultation_successfully');
 const headerConsultationFormButton = document.querySelector('.header-consultation-form__button');
+const greenTariffLink = document.querySelector('.green-tariff__link');
+const greenTariffContentBlocks = document.querySelectorAll('.green-tariff-content-block');
+
+// Select content relative to links on green tariff section
+function green_tariff_decoration() {
+
+    for (let elem of greenTariffLink.children) {
+
+        elem.addEventListener('click', function () {
+            let linkNum = elem.dataset.linknum;
+
+            for (let value of greenTariffLink.children) {
+                value.classList.remove('underline');
+            }
+            elem.classList.add('underline');
+
+            for (let block of greenTariffContentBlocks) {
+
+                if (block.dataset.linknum == linkNum) {
+                    block.style.display = 'block';
+                } else {
+                    block.style.display = 'none';
+                }
+            }
+        })
+    }
+};
 
 // Show or hide consultation form on click
 showFormButton.addEventListener('click', function () {
@@ -24,3 +51,8 @@ headerConsultationFormButton.addEventListener('click', function () {
         headerSuccessfully.style.display = 'none';
     }, 10000)
 });
+
+let firstChild = greenTariffLink.children;
+firstChild[0].classList.add('underline');
+
+green_tariff_decoration();
